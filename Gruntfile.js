ec80f894e1962b0320be49eca9950beb22f510b6
Svetlin Mladenov <svetlin.mladenov@gmail.com>
2015-06-08 16:25:55 +0300

    Add grunt task 'viblast-vast' that packs and minifies all source files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,18 @@ var specs = "spec/*Spec.js",
 
 var _ = require('lodash');
 
+var src = [
+  "videojs.viblast.js",
+  "lib/videojs-contrib-ads/videojs.ads.js",
+  "lib/vast-client.js",
+  "videojs.vast.js",
+];
+
+var src_css = [
+  "lib/videojs-contrib-ads/videojs.ads.css",
+  "videojs.vast.css",
+];
+
 var gruntConfig = {
   env: {
     // dynamically filled
@@ -53,7 +65,20 @@ var gruntConfig = {
       files: ['*.js', 'spec/*.js'],
       tasks: ['jshint', 'jasmine']
     },
-  }
+  },
+  uglify: {
+    viblast_vast: {
+      files: {
+        'dist/videojs.viblast-vast.js': src
+      }
+    }
+  },
+  concat: {
+	viblast_vast_css: {
+	  src: src_css,
+	  dest: 'dist/videojs.viblast-vast.css',
+	},
+  },
 };
 
 module.exports = function(grunt) {
@@ -66,12 +91,17 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-karma-coveralls');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['test:sauce:' + _(desireds).keys().first()]);
-
-  _(desireds).each(function(desired, key) {
-    grunt.registerTask('test:sauce:' + key, ['env:' + key, 'simplemocha:sauce']);
-  });
+  if (typeof desireds !== 'undefined') {
+	  grunt.registerTask('default', ['test:sauce:' + _(desireds).keys().first()]);
+	
+	  _(desireds).each(function(desired, key) {
+	    grunt.registerTask('test:sauce:' + key, ['env:' + key, 'simplemocha:sauce']);
+	  });
+  }
 
   grunt.registerTask('default', ['jshint', 'karma']);
+  grunt.registerTask('viblast-vast', ['uglify:viblast_vast', 'concat:viblast_vast_css']);
 };
