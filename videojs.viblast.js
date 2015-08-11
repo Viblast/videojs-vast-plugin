7fa2183cc8c1ec3a23f94a6034748d7335e052fa
Nikolay Dimitrov <nikolay.dimitrov@viblast.com>
2015-08-11 17:56:51 +0300

    Migrated src overriding to Viblast techs

--- videojs.viblast.js
+++ videojs.viblast.js
@@ -5,16 +5,6 @@
 "use strict";
 
 var ViblastPlugin = function() {
-	var player = this;
-	var overridenCurrentSrc = player.currentSrc;
-	player.currentSrc = function() {
-		var vid = document.querySelector('#' + player.id() + ' video');
-		if (vid.dataset['viblastSrc']) {
-			return vid.dataset['viblastSrc'];
-		} else {
-			return overridenCurrentSrc.call(this);
-		}
-	};
 };
 
 vjs.plugin('viblast', ViblastPlugin);
